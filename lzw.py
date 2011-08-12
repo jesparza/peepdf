123f7ae4a57e07c65949b41dc126d9dbd71bde7e
josemiguel.esparza <josemiguel.esparza@092d1cd0-dfcb-bb13-d408-b3417c557306>
2011-08-12 16:43:20 +0000

    Added lzwdecode from pdfminer to improve code and avoid bugs

--- lzw.py
+++ lzw.py
@@ -832,3 +832,114 @@ def bitstobytes(bits):
     if nextbit < 7: ret.append(nextbyte)
     return ret
         
+
+
+
+'''
+The code below is part of pdfminer (http://pypi.python.org/pypi/pdfminer/)
+
+Copyright (c) 2004-2010 Yusuke Shinyama <yusuke at cs dot nyu dot edu>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+'''
+
+import sys
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+
+##  LZWDecoder
+##
+class LZWDecoder(object):
+
+    debug = 0
+
+    def __init__(self, fp):
+        self.fp = fp
+        self.buff = 0
+        self.bpos = 8
+        self.nbits = 9
+        self.table = None
+        self.prevbuf = None
+        return
+
+    def readbits(self, bits):
+        v = 0
+        while 1:
+            # the number of remaining bits we can get from the current buffer.
+            r = 8-self.bpos
+            if bits <= r:
+                # |-----8-bits-----|
+                # |-bpos-|-bits-|  |
+                # |      |----r----|
+                v = (v<<bits) | ((self.buff>>(r-bits)) & ((1<<bits)-1))
+                self.bpos += bits
+                break
+            else:
+                # |-----8-bits-----|
+                # |-bpos-|---bits----...
+                # |      |----r----|
+                v = (v<<r) | (self.buff & ((1<<r)-1))
+                bits -= r
+                x = self.fp.read(1)
+                if not x: raise EOFError
+                self.buff = ord(x)
+                self.bpos = 0
+        return v
+
+    def feed(self, code):
+        x = ''
+        if code == 256:
+            self.table = [ chr(c) for c in xrange(256) ] # 0-255
+            self.table.append(None) # 256
+            self.table.append(None) # 257
+            self.prevbuf = ''
+            self.nbits = 9
+        elif code == 257:
+            pass
+        elif not self.prevbuf:
+            x = self.prevbuf = self.table[code]
+        else:
+            if code < len(self.table):
+                x = self.table[code]
+                self.table.append(self.prevbuf+x[0])
+            else:
+                self.table.append(self.prevbuf+self.prevbuf[0])
+                x = self.table[code]
+            l = len(self.table)
+            if l == 511:
+                self.nbits = 10
+            elif l == 1023:
+                self.nbits = 11
+            elif l == 2047:
+                self.nbits = 12
+            self.prevbuf = x
+        return x
+
+    def run(self):
+        while 1:
+            try:
+                code = self.readbits(self.nbits)
+            except EOFError:
+                break
+            x = self.feed(code)
+            yield x
+            if self.debug:
+                print >>sys.stderr, ('nbits=%d, code=%d, output=%r, table=%r' %
+                                     (self.nbits, code, x, self.table[258:]))
+        return
+
+
+def lzwdecode(data):
+    """
+    >>> lzwdecode('\x80\x0b\x60\x50\x22\x0c\x0c\x85\x01')
+    '\x2d\x2d\x2d\x2d\x2d\x41\x2d\x2d\x2d\x42'
+    """
+    fp = StringIO(data)
+    return ''.join(LZWDecoder(fp).run())
