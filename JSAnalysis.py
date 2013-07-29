714073b3774c44be986f5d99766e8d86f6ac3297
Jose Miguel Esparza <jesparza@users.noreply.github.com>
2013-07-29 22:39:20 +0000

    Added PyV8 as Javascript engine and removed Spidermonkey. Now hooking the eval function to obtain the Javascript code. Also added option to avoid Javascript execution.

--- JSAnalysis.py
+++ JSAnalysis.py
@@ -1,56 +1,66 @@
 #
-#	peepdf is a tool to analyse and modify PDF files
-#	http://peepdf.eternal-todo.com
-#	By Jose Miguel Esparza <jesparza AT eternal-todo.com>
+#    peepdf is a tool to analyse and modify PDF files
+#    http://peepdf.eternal-todo.com
+#    By Jose Miguel Esparza <jesparza AT eternal-todo.com>
 #
-#	Copyright (C) 2012 Jose Miguel Esparza
+#    Copyright (C) 2011-2013 Jose Miguel Esparza
 #
-#	This file is part of peepdf.
+#    This file is part of peepdf.
 #
-#		peepdf is free software: you can redistribute it and/or modify
-#		it under the terms of the GNU General Public License as published by
-#		the Free Software Foundation, either version 3 of the License, or
-#		(at your option) any later version.
+#        peepdf is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        (at your option) any later version.
 #
-#		peepdf is distributed in the hope that it will be useful,
-#		but WITHOUT ANY WARRANTY; without even the implied warranty of
-#		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
-#		GNU General Public License for more details.
+#        peepdf is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+#        GNU General Public License for more details.
 #
-#		You should have received a copy of the GNU General Public License
-#		along with peepdf.	If not, see <http://www.gnu.org/licenses/>.
+#        You should have received a copy of the GNU General Public License
+#        along with peepdf.    If not, see <http://www.gnu.org/licenses/>.
 #
 
 '''
     This module contains some functions to analyse Javascript code inside the PDF file
 '''
 
-import sys, re , os, jsbeautifier
-from PDFUtils import unescapeHTMLEntities
+import sys, re , os, jsbeautifier, traceback
+from PDFUtils import unescapeHTMLEntities, escapeString
 try:
-	from spidermonkey import Runtime
-	JS_MODULE = True 
+    import PyV8
+    JS_MODULE = True 
 except:
-	JS_MODULE = False
+    JS_MODULE = False
+
+class Global(PyV8.JSClass):
+    evalCode = ''
+    
+    def evalOverride(self, expression):
+        self.evalCode += '\n\n// New evaluated code\n' + expression
+        return
+
 newLine = os.linesep         
 reJSscript = '<script[^>]*?contentType\s*?=\s*?[\'"]application/x-javascript[\'"][^>]*?>(.*?)</script>'
- 
-def analyseJS(code):
+preDefinedCode = 'var app = this;'
+
+def analyseJS(code, context = None, manualAnalysis = False):
     '''
-        Search for obfuscated functions in the Javascript code
+        Hooks the eval function and search for obfuscated elements in the Javascript code
         
         @param code: The Javascript code (string)
-        @return: List with analysis information of the Javascript code: [JSCode,unescapedBytes,urlsFound], where JSCode is a list with the several stages Javascript code, unescapedBytes is a list with the parameters of unescape functions, and urlsFound is a list with the URLs found in the unescaped bytes. 
+        @return: List with analysis information of the Javascript code: [JSCode,unescapedBytes,urlsFound,errors,context], where 
+                JSCode is a list with the several stages Javascript code,
+                unescapedBytes is a list with the parameters of unescape functions, 
+                urlsFound is a list with the URLs found in the unescaped bytes,
+                errors is a list of errors,
+                context is the context of execution of the Javascript code.
     '''
-    error = ''
     errors = []
     JSCode = []
     unescapedBytes = []
     urlsFound = []
-    oldStdErr = sys.stderr
-    errorFile = open('jserror.log','wb')
-    sys.stderr = errorFile
-	
+    
     try:
         scriptCode = re.findall(reJSscript, code, re.DOTALL | re.IGNORECASE)
         if scriptCode != []:
@@ -59,49 +69,35 @@ def analyseJS(code):
         code = jsbeautifier.beautify(code)
         JSCode.append(code)
     
-        if code != None and JS_MODULE:
-            r = Runtime()
-            context = r.new_context()
+        if code != None and JS_MODULE and not manualAnalysis:
+            if context == None:
+                context = PyV8.JSContext(Global())
+            context.enter()
+            # Hooking the eval function
+            context.eval('eval=evalOverride')
+            #context.eval(preDefinedCode)
             while True:
-                evalFunctionsData = searchObfuscatedFunctions(code, 'eval')
-                originalElement = code
-                for evalFunctionData in evalFunctionsData:
-                    if not evalFunctionData[2]:
-                        modifiedCode = evalFunctionData[1][0].replace(evalFunctionData[0],'return')
-                        code = originalElement.replace(evalFunctionData[1][0],modifiedCode)
+                originalCode = code
+                try:
+                    context.eval(code)
+                    evalCode = context.eval('evalCode')
+                    evalCode = jsbeautifier.beautify(evalCode)
+                    if evalCode != '' and evalCode != code:
+                        code = evalCode
+                        JSCode.append(code)
                     else:
-                        code = originalElement.replace(evalFunctionData[1][0],evalFunctionData[1][1]+';')
-                    try:
-                        executedJS = context.eval_script(code)
-                        if executedJS == None:
-                            raise exception
                         break
-                    except:                   
-                        if evalFunctionData[2]:
-                            modifiedCode = evalFunctionData[1][0].replace(evalFunctionData[0],'return')
-                            code = originalElement.replace(evalFunctionData[1][0],modifiedCode)
-                        else:
-                            code = originalElement.replace(evalFunctionData[1][0],evalFunctionData[1][1]+';')
-                        try:
-                            executedJS = context.eval_script(code)
-                            if executedJS == None:
-                                raise exception
-                        except:
-                            code = originalElement
-                            continue
-                else:
-                    break
-                if executedJS != originalElement and executedJS != None and executedJS != '':
-                    code = executedJS
-                    JSCode.append(code)                
-                else:                                            
+                except:
+                    error = str(sys.exc_info()[1])
+                    open('jserror.log','ab').write(error + newLine)
+                    errors.append(error)
                     break
             
-            if code != None:
+            if code != '':
                 escapedVars = re.findall('(\w*?)\s*?=\s*?(unescape\((.*?)\))', code, re.DOTALL)
                 for var in escapedVars:
                     bytes = var[2]
-                    if bytes.find('+') != -1:
+                    if bytes.find('+') != -1 or bytes.find('%') == -1:
                         varContent = getVarContent(code, bytes)
                         if len(varContent) > 150:
                             ret = unescape(varContent)
@@ -126,29 +122,22 @@ def analyseJS(code):
                                    if url not in urlsFound:
                                        urlsFound.append(url)
     except:
-        errors.append('Unknown error!!')
+        traceback.print_exc(file=open(errorsFile,'a'))
+        errors.append('Unexpected error in the JSAnalysis module!!')
     finally:
-        errorFile.close()
-        sys.stderr = oldStdErr
-        errorFileContent = open('jserror.log','rb').read()
-        if errorFileContent != '' and errorFileContent.find('JavaScript error') != -1:
-            lines = errorFileContent.split(newLine)
-            for line in lines:
-                if line.find('JavaScript error') != -1 and line not in errors:
-                    errors.append(line)
         for js in JSCode:
             if js == None or js == '':
                  JSCode.remove(js)
-    return [JSCode,unescapedBytes,urlsFound,errors]
-       
+    return [JSCode,unescapedBytes,urlsFound,errors,context]
+ 
 def getVarContent(jsCode, varContent):
     '''
-		Given the Javascript code and the content of a variable this method tries to obtain the real value of the variable, cleaning expressions like "a = eval; a(js_code);"
-		
-		@param jsCode: The Javascript code (string)
-		@param varContent: The content of the variable (string)
-		@return: A string with real value of the variable
-	'''
+        Given the Javascript code and the content of a variable this method tries to obtain the real value of the variable, cleaning expressions like "a = eval; a(js_code);"
+        
+        @param jsCode: The Javascript code (string)
+        @param varContent: The content of the variable (string)
+        @return: A string with real value of the variable
+    '''
     clearBytes = ''
     varContent = varContent.replace('\n','')
     varContent = varContent.replace('\r','')
@@ -159,6 +148,7 @@ def getVarContent(jsCode, varContent):
         if re.match('["\'].*?["\']', part, re.DOTALL):
             clearBytes += part[1:-1]
         else:
+            part = escapeString(part)
             varContent = re.findall(part + '\s*?=\s*?(.*?)[,;]', jsCode, re.DOTALL)
             if varContent != []:
                 clearBytes += getVarContent(jsCode, varContent[0])
@@ -200,25 +190,25 @@ def isJavascript(content):
     
 def searchObfuscatedFunctions(jsCode, function):
     '''
-		Search for obfuscated functions in the Javascript code
-		
-		@param jsCode: The Javascript code (string)
-		@param function: The function name to look for (string)
-		@return: List with obfuscated functions information [functionName,functionCall,containsReturns] 
-	'''
+        Search for obfuscated functions in the Javascript code
+        
+        @param jsCode: The Javascript code (string)
+        @param function: The function name to look for (string)
+        @return: List with obfuscated functions information [functionName,functionCall,containsReturns] 
+    '''
     obfuscatedFunctionsInfo = []
     if jsCode != None:
-	    match = re.findall('\W('+function+'\s{0,5}?\((.*?)\)\s{0,5}?;)', jsCode, re.DOTALL)
-	    if match != []:
-	       for m in match:
-	          if re.findall('return',m[1],re.IGNORECASE) != []:
-	             obfuscatedFunctionsInfo.append([function,m,True])
-	          else:
-	             obfuscatedFunctionsInfo.append([function,m,False])
-	    obfuscatedFunctions = re.findall('\s*?((\w*?)\s*?=\s*?'+function+')\s*?;', jsCode, re.DOTALL)
-	    for obfuscatedFunction in obfuscatedFunctions:
-	       obfuscatedElement = obfuscatedFunction[1]
-	       obfuscatedFunctionsInfo += searchObfuscatedFunctions(jsCode, obfuscatedElement)
+        match = re.findall('\W('+function+'\s{0,5}?\((.*?)\)\s{0,5}?;)', jsCode, re.DOTALL)
+        if match != []:
+           for m in match:
+              if re.findall('return',m[1],re.IGNORECASE) != []:
+                 obfuscatedFunctionsInfo.append([function,m,True])
+              else:
+                 obfuscatedFunctionsInfo.append([function,m,False])
+        obfuscatedFunctions = re.findall('\s*?((\w*?)\s*?=\s*?'+function+')\s*?;', jsCode, re.DOTALL)
+        for obfuscatedFunction in obfuscatedFunctions:
+           obfuscatedElement = obfuscatedFunction[1]
+           obfuscatedFunctionsInfo += searchObfuscatedFunctions(jsCode, obfuscatedElement)
     return obfuscatedFunctionsInfo
 
 def unescape(escapedBytes, unicode = True):
