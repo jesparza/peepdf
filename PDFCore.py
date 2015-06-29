a396c6343e3460a258756314e452f9e2d45adc68
Rohit Dua <8ohit.dua@gmail.com>
2015-06-30 01:02:45 +0530

    string obfuscation check before octal conv.

--- PDFCore.py
+++ PDFCore.py
@@ -659,6 +659,14 @@ class PDFString (PDFObject) :
             errorMessage = 'Error in octal conversion'
             self.addError(errorMessage)
             return (-1,errorMessage)
+        rawValue = str(self.rawValue)
+        newValue = str(self.value)
+        if newValue != rawValue:
+            self.stringObfuscated = True
+        else:
+            self.stringObfuscated = False
+        if len(rawValue) > MAX_STR_LEN:
+            self.largeStringPresent = True
         if isJavascript(self.value):
             self.containsJScode = True
             self.JSCode, self.unescapedBytes, self.urlsFound, jsErrors, jsContexts['global'] = analyseJS(self.value, jsContexts['global'], isManualAnalysis)
@@ -673,14 +681,6 @@ class PDFString (PDFObject) :
             ret = self.encrypt()
             if ret[0] == -1:
                 return ret
-        rawValue = str(self.rawValue)
-        newValue = str(self.value)
-        if newValue != rawValue:
-            self.stringObfuscated = True
-        else:
-            self.stringObfuscated = False
-        if len(newValue) > MAX_STR_LEN:
-            self.largeStringPresent = True
         return (0,'')
 
     def encodeChars(self):
