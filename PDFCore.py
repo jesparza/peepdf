c7f948c997f79d6baf1cc020a6d2e667fc29d6ea
Rohit Dua <8ohit.dua@gmail.com>
2015-07-02 05:07:07 +0530

    getIsolatedObjects update: objectType check

--- PDFCore.py
+++ PDFCore.py
@@ -6118,6 +6118,7 @@ class PDFFile :
         catalogIdLinear = None
         infoIdLinear = None
         catalogLinear = None
+        objectTypeList = ['dictionary', 'array', 'stream']
         for version in range(self.updates+1):
             catalogId = None
             infoId = None
@@ -6151,7 +6152,7 @@ class PDFFile :
                 for objectId in isolatedList:
                     indirectObj = self.getObject(objectId, indirect=True)
                     object = indirectObj.getObject()
-                    if object.hasElement('/Linearized'):
+                    if object.getType() in objectTypeList and object.hasElement('/Linearized'):
                         continue
                     object.missingCatalog = True
                     self.body[version].deregisterObject(indirectObj)
@@ -6173,7 +6174,7 @@ class PDFFile :
                             isolatedListDict[version].append(objectId)
                         indirectObj = self.getObject(objectId, indirect=True)
                         object = indirectObj.getObject()
-                        if object.hasElement('/Linearized'):
+                        if object.getType() in objectTypeList and object.hasElement('/Linearized'):
                             continue
                         object.missingCatalog = True
                         self.body[version].deregisterObject(indirectObj)
