270551bf6cea6a1caa14388f0d45301a4f437b2d
Jose Miguel Esparza <jesparza@users.noreply.github.com>
2015-11-08 23:10:39 +0100

    Fixed PDFFile.getCatalogObject to get the correct Catalog objects per document version

--- PDFCore.py
+++ PDFCore.py
@@ -5594,13 +5594,14 @@ class PDFFile :
                         break
         return basicMetadata
     
-    def getCatalogObject(self, version = None, indirect = False):
+    def getCatalogObject(self, version=None, indirect=False):
         if version == None:
             catalogObjects = []
             catalogIds = self.getCatalogObjectId()
-            for id in catalogIds:
+            for i in xrange(len(catalogIds)):
+                id = catalogIds[i]
                 if id != None:
-                    catalogObject = self.getObject(id, version, indirect)
+                    catalogObject = self.getObject(id, i, indirect)
                     catalogObjects.append(catalogObject)
                 else:
                     catalogObjects.append(None)
