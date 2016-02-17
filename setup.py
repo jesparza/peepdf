32dff1302f2678268556500a08db0aa43ebbf95d
Thibault Marquand <titotix@free.fr>
2016-02-17 18:17:43 +0100

    setup.py: module in lower case

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = "PeePDF",
+    name = "peepdf",
     version = "0.3-r235",
     author = "Jose Miguel Esparza",
     license = "GNU GPLv3",
