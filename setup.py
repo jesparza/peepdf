156d6185b15d9474dd4e99975daa512bd0bb16fa
Thibault Marquand <titotix@free.fr>
2016-02-17 13:20:46 +0100

    intoduce setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "PeePDF",
+    version = "0.3-r235",
+    author = "Jose Miguel Esparza",
+    license = "GNU GPLv3",
+    url = "http://eternal-todo.com",
+    packages = find_packages(),
+)
