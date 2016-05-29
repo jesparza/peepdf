adc512cbdeff9c908bfcfdb7318577ee637003ec
Thibault Marquand <titotix@free.fr>
2016-05-29 23:38:11 +0200

    aespython as external lib

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author = "Jose Miguel Esparza",
     license = "GNU GPLv3",
     url = "http://eternal-todo.com",
-    install_requires = [ "jsbeautifier==1.6.2", "colorama" ],
+    install_requires = [ "jsbeautifier==1.6.2", "colorama", "pythonaes==1.0" ],
+    dependency_links = ["git+https://github.com/serprex/pythonaes.git#egg=pythonaes-1.0"],
     packages = find_packages(),
 )
