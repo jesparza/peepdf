801a057bb01e5bea6545ad65e8991bfef562ba71
Jurriaan Bremer <jbr@cuckoo.sh>
2016-06-30 12:34:44 +0200

    pep8 setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = "peepdf",
-    version = "0.3-r275",
-    author = "Jose Miguel Esparza",
-    license = "GNU GPLv3",
-    url = "http://eternal-todo.com",
-    install_requires = [ "jsbeautifier==1.6.2", "colorama", "Pillow", "pythonaes==1.0", "lxml" ],
-    dependency_links = ["git+https://github.com/serprex/pythonaes.git#egg=pythonaes-1.0"],
-    packages = find_packages(),
+    name="peepdf",
+    version="0.3-r275",
+    author="Jose Miguel Esparza",
+    license="GNU GPLv3",
+    url="http://eternal-todo.com",
+    install_requires=[
+        "jsbeautifier==1.6.2",
+        "colorama",
+        "Pillow",
+        "pythonaes==1.0",
+        "lxml",
+    ],
+    dependency_links=[
+        "git+https://github.com/serprex/pythonaes.git#egg=pythonaes-1.0",
+    ],
+    packages=find_packages(),
 )
