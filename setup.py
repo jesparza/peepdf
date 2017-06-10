80748f50d833f0b33ebb56ed5501f82e0f2bc2f8
Jurriaan Bremer <jbr@cuckoo.sh>
2017-06-10 23:38:57 +0200

    version 0.3.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="peepdf",
-    version="0.3.5",
+    version="0.3.6",
     author="Jose Miguel Esparza",
     license="GNU GPLv3",
     url="http://eternal-todo.com",
