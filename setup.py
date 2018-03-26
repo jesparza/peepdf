a63d628ad8b58d23e8b39adc3ca01b91eb275e3c
Jurriaan Bremer <jbr@cuckoo.sh>
2018-03-26 18:19:59 +0200

    version 0.4.1 with future included

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name="peepdf",
-    version="0.4.0",
+    version="0.4.1",
     author="Jose Miguel Esparza",
     license="GNU GPLv3",
     url="http://eternal-todo.com",
     install_requires=[
         "jsbeautifier==1.6.2",
         "colorama==0.3.7",
-        "future",
+        "future>=0.16.0",
         "Pillow==3.2.0",
         "pythonaes==1.0",
     ],
