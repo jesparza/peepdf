7a517882252831f1c6e6cb64c3a611a0aed6ecd5
tholep <tho.le3839@gmail.com>
2018-11-04 22:47:05 +0100

    complete JSUnpack function to analyse JS with different viewerversion and added data from info object + name fields from XML --> move to test phase

--- JSAnalysis.py
+++ JSAnalysis.py
@@ -30,7 +30,11 @@ import os
 import re
 import sys
 import traceback
-
+import xml.dom.minidom
+import subprocess
+from subprocess import PIPE
+import random
+import string
 from PDFUtils import unescapeHTMLEntities, escapeString
 
 try:
@@ -54,13 +58,14 @@ newLine = os.linesep
 reJSscript = '<script[^>]*?contentType\s*?=\s*?[\'"]application/x-javascript[\'"][^>]*?>(.*?)</script>'
 preDefinedCode = 'var app = this;'
 
-def JSUnpack(code,infoObject,manualAnalysis=False):
+def JSUnpack(code,rawCode,infoObjects,manualAnalysis=False):
     '''
     Hooks the eval function with multiple app versions and search for obfuscated elements in the Javascript code.
     Also take data from XFA, object info and getAnnot(s) in a PDF to an original code. The idea is mainly taken from JSUnpack
     
-    @param code: The Javascript code (string)
-    @param infoObject: is infoObject of a PDF
+    @param code: the Javascript code (string)
+    @param rawCode: The raw Javascript code, may contains HTML, XML elements (string)
+    @param infoObjects: is list of infoObjects of a PDF
     @param manualAnalysis: analyse manually or automatic (boolean)
     @return: List with analysis information of the Javascript code: [JSCode,unescapedBytes,urlsFound,errors], where 
             JSCode is a list with the several stages Javascript code,
@@ -74,54 +79,181 @@ def JSUnpack(code,infoObject,manualAnalysis=False):
     urlsFound = []
     #pre-code with data from inforamtion object
     preInfo=''
-    #Take variable name(s) of xml elements in XFA
-    preXFAVar=''
+    #Take variable name(s) of xml elements (.e.g in XFA, Acroform)
+    preXMLVar=[]
+    XMLVar=''
     #Build page tree and annotation data
     preAnnot=''
+    #version strings
+    pdfVersions = ['','7.0','8.0','9.1']
 
     #get preInfo from InfoObject
-    infoObject=pdf.getInfoObject()
-    for obj in infoObject:
+    for obj in infoObjects:
         elements=obj.getElements()
         if elements.has_key("/Creator"):
             creatorValue=elements["/Creator"].getValue()
-            preInfo +="info.creator = String(%s);" % (str(creatorValue))
-            preInfo +="this.creator = info.creator;"
-            preInfo +="info.Creator = info.creator;"
-            preInfo +="app.doc.creator = info.creator;"
-            preInfo +="app.doc.Creator = info.creator;"
+            preInfo +='info.creator = String("%s");\n' % (str(creatorValue))
+            preInfo +="this.creator = info.creator;\n"
+            preInfo +="info.Creator = info.creator;\n"
+            preInfo +="app.doc.creator = info.creator;\n"
+            preInfo +="app.doc.Creator = info.creator;\n"
         if elements.has_key("/Title"):
-            titleValue=elements["/Creator"].getValue()
-            preInfo +="info.title = string(%s);" % (str(titleValue))
-            preInfo +="this.title = info.title;"
-            preInfo +="info.Title = info.title;"
-            preInfo +="app.doc.title = info.title;"
-            preInfo +="app.doc.Title = info.title;"
+            titleValue=elements["/Title"].getValue()
+            preInfo +='info.title = string("%s");\n' % (str(titleValue))
+            preInfo +="this.title = info.title;\n"
+            preInfo +="info.Title = info.title;\n"
+            preInfo +="app.doc.title = info.title;\n"
+            preInfo +="app.doc.Title = info.title;\n"
         if elements.has_key("/Subject"):
             subjectValue=elements["/Subject"].getValue()
-            preInfo +="info.subject = String(%s);" % (str(subjectValue))
-            preInfo +="this.subject = info.subject;"
-            preInfo +="info.Subject = info.subject;"
-            preInfo +="app.doc.subject = info.subject;"
-            preInfo +="app.doc.Subject = info.subject;"
+            preInfo +='info.subject = String("%s");\n' % (str(subjectValue))
+            preInfo +="this.subject = info.subject;\n"
+            preInfo +="info.Subject = info.subject;\n"
+            preInfo +="app.doc.subject = info.subject;\n"
+            preInfo +="app.doc.Subject = info.subject;\n"
         if elements.has_key("/Author"):
             authorValue=elements["/Author"].getValue()
-            preInfo +="info.author = String(%s);" % (str(authorValue))
-            preInfo +="this.author = info.author;"
-            preInfo +="info.Author = info.author;"
-            preInfo +="app.doc.author = info.author;"
-            preInfo +="app.doc.Author = info.author;"
+            preInfo +='info.author = String("%s");\n' % (str(authorValue))
+            preInfo +="this.author = info.author;\n"
+            preInfo +="info.Author = info.author;\n"
+            preInfo +="app.doc.author = info.author;\n"
+            preInfo +="app.doc.Author = info.author;\n"
         if elements.has_key("/CreationDate"):
             dateValue=elements["/CreationDate"].getValue()
-            preInfo +="info.creationdate = String(%s);" % (str(dateValue))
-            preInfo +="this.creationdate = info.creationdate;"
-            preInfo +="info.CreationDate = info.creationdate;"
-            preInfo +="app.doc.creationdate = info.creationdate;"
-            preInfo +="app.doc.CreationDate = info.creationdate;"
-            preInfo +="app.doc.creationDate = info.creationdate;"
-            preInfo +="info.creationDate = info.creationdate;"
-    
+            preInfo +='info.creationdate = String("%s");\n' % (str(dateValue))
+            preInfo +="this.creationdate = info.creationdate;\n"
+            preInfo +="info.CreationDate = info.creationdate;\n"
+            preInfo +="app.doc.creationdate = info.creationdate;\n"
+            preInfo +="app.doc.CreationDate = info.creationdate;\n"
+            preInfo +="app.doc.creationDate = info.creationdate;\n"
+            preInfo +="info.creationDate = info.creationdate;\n:"
+
     #Get xml variable name
+    try:
+        doc = xml.dom.minidom.parseString(rawCode)
+        scriptElements = doc.getElementsByTagNameNS("*", "script")
+        if scriptElements:
+            for script in scriptElements:
+                nameVar= script.parentNode.parentNode.getAttribute('name')
+                if nameVar:
+                    XMLVar += nameVar + " = this;\n"
+    
+    except Exception as e:
+        pass
+    
+    #Pre-process input code, same as in analyseJS
+    try:
+        code = unescapeHTMLEntities(code)
+        scriptElements = re.findall(reJSscript, code, re.DOTALL | re.IGNORECASE)
+        if scriptElements:
+            code = ''
+            for scriptElement in scriptElements:
+                code += scriptElement + '\n\n'
+        code = jsbeautifier.beautify(code)
+        jsCode.append(code)
+
+
+        if code is not None and not manualAnalysis:
+            for version in pdfVersions:
+                viewerVersion='app.viewerVersion = Number(%s);\n' % (version)
+                while True:
+                    originalCode = code
+                    code = viewerVersion + preInfo + XMLVar + code
+
+                    
+                    if code != '':
+                        #Detect shellcode and embedded URL(s) in case of using unescape function. e.g. x = unescape(%u0A0A%0B0B)
+                        escapedVars = re.findall('(\w*?)\s*?=\s*?(unescape\((.*?)\))', code, re.DOTALL)
+                        for var in escapedVars:
+                            bytes = var[2]
+                            if bytes.find('+') != -1 or bytes.find('%') == -1:
+                                varContent = getVarContent(code, bytes)
+                                if len(varContent) > 150:
+                                    ret = unescape(varContent)
+                                    if ret[0] != -1:
+                                        bytes = ret[1]
+                                        urls = re.findall('https?://.*$', bytes, re.DOTALL)
+                                        if bytes not in unescapedBytes:
+                                            unescapedBytes.append(bytes)
+                                        for url in urls:
+                                            if url not in urlsFound:
+                                                urlsFound.append(url)
+                            else:
+                                bytes = bytes[1:-1]
+                                if len(bytes) > 150:
+                                    ret = unescape(bytes)
+                                    if ret[0] != -1:
+                                        bytes = ret[1]
+                                        urls = re.findall('https?://.*$', bytes, re.DOTALL)
+                                        if bytes not in unescapedBytes:
+                                            unescapedBytes.append(bytes)
+                                        for url in urls:
+                                            if url not in urlsFound:
+                                                urlsFound.append(url)
+                        # Detect shellcode in case of finding variable assigned to an escaped string
+                        # post.js produce a signature. e.g. #//shellcode len 767 (including any NOPs) payload = %u0A0A%u0A0A%u0A0A%uE1D9%u34D9%u5824%u5858
+                        escapedVars = re.findall('//shellcode (pdf|len) (\d+) .*? = (.*)$', code,re.DOTALL)
+                        for var in escapedVars:
+                            bytes = var[2]
+                            if len(bytes) > 150:
+                                ret = unescape(bytes)
+                                if ret[0] != -1:
+                                    bytes = ret[1]
+                                    urls = re.findall('https?://.*$', bytes, re.DOTALL)
+                                    if bytes not in unescapedBytes:
+                                        unescapedBytes.append(bytes)
+                                    for url in urls:
+                                        if url not in urlsFound:
+                                            urlsFound.append(url)
+
+
+                    #Hook eval and run Javascript
+                    evalCode,error = evalJs(code)
+                    evalCode = jsbeautifier.beautify(evalCode)
+                    if error != "":
+                        errors.append(error)
+
+                    #if next stage of the JS exists, re-eval the next stage
+                    if evalCode != '' and evalCode != code:
+                        code = evalCode
+                        jsCode.append(code)
+                    else:
+                        break
+            
+    except:
+        traceback.print_exc(file=open(errorsFile, 'a'))
+        errors.append('Unexpected error in the JSUnpack module!!')
+    finally:
+        for js in jsCode:
+            if js is None or js == '':
+                 jsCode.remove(js)
+ 
+    return [jsCode, unescapedBytes, urlsFound, errors, context]
+
+def evalJS(code):
+    """
+    @param code: the Javascript code
+    @return: a set of eval code, error
+    """
+    try: 
+        fileJS = randomString(10)
+        with open(fileJS,'w') as fileJS:
+            fileJS.write(code)
+
+        po = subprocess.Popen(['v8', '-f', "pre.js", '-f', fileJS + '.js', '-f', "post.js"],shell=False, stdout=PIPE, stderr=PIPE)
+        return (po.stdout.read(),po.stderr.read())
+    except:
+        error = str(sys.exc_info()[1])
+        open('jserror.log', 'ab').write(error + newLine)
+        errors.append(error)
+        return ("",error)
+        
+    
+    
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
 def analyseJS(code, context=None, manualAnalysis=False):
     '''
@@ -347,3 +479,24 @@ def unescape(escapedBytes, unicode = True):
     except:
         return (-1, 'Error while unescaping the bytes')
     return (0, unescapedBytes)
+
+
+if __name__ == "__main__":
+    from PDFCore import *
+    from JSAnalysis import *
+    #create object pdf
+    from PDFCore import *
+
+    fileName="/home/thole/Documents/samples/pdf_samples/AAAA/sample3/ffe8db8803d5ead7a7c4d4dfd393e4601a91b867"
+    pdfParser = PDFParser()
+    ret, pdf = pdfParser.parse(fileName, True,True,False)
+    infoObjects=pdf.getInfoObject()
+
+    rawCode=pdf.getObject(1)
+    code=rawCode.getJSCode()[0]
+    if rawCode.getType() == "stream":
+            rawCode=rawCode.getStream()
+    else:
+            rawCode=rawCode.getValue()
+
+    JSUnpack(rawCode,code,infoObjects)
