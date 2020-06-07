a806001ab0a288aaee50240569e45a8345766a32
Corey <corey@digitalsleuth.ca>
2020-06-07 20:36:42 +0000

    Fix setup.py

--- peepdf/__init__.py
+++ peepdf/__init__.py
@@ -19,4 +19,4 @@
 #        You should have received a copy of the GNU General Public License
 #        along with peepdf.    If not, see <http://www.gnu.org/licenses/>.
 
-from peepdf import PDFConsole, PDFCore, PDFCrypto, PDFFilters, PDFUtils, JSAnalysis
+from . import PDFConsole, PDFCore, PDFCrypto, PDFFilters, PDFUtils, JSAnalysis
